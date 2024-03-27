8ca2455609e635e0de1e958f9c5433ec557aa8fe
farhan-rajput <mfs.khi@gmail.com>
2024-03-27 14:13:25 +0500

    add Guessing number game files

---
+++ CLI-Guessing-Number/index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// 1). Computer will generate a random number.-----Done 
+// 2). user input for guessing a random number.----Done
+// 3). compare computer input with user input.-----Done
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+// console.log (randomNumber)
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1 to 6: ",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("congratulation! you guessed right number.");
+}
+else {
+    console.log("you guessed wrong number");
+}
